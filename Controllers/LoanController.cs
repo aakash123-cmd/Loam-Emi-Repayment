c3e7f971112e21fe794710b1418b28591d9e5fe1
aakash123-cmd <aakashashtekar232@gmail.com>
2025-11-27 12:39:00 +0530

    Completed all loan logic

--- Controllers/LoanController.cs
+++ Controllers/LoanController.cs
@@ -5,12 +5,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Loan___Emi_Repayment.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class LoanController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
         public LoanController(IUnitOfWork unitOfWork)
         {
-        _unitOfWork = unitOfWork;
+            _unitOfWork = unitOfWork;
         }
 
 
@@ -33,13 +35,28 @@ namespace Loan___Emi_Repayment.Controllers
         {
             if (!ModelState.IsValid) return ValidationProblem(ModelState);
 
-            if (ph.AmountPaid <= 0)return BadRequest("Amount must be greater than zero");
+            if (ph.AmountPaid <= 0) return BadRequest("Amount must be greater than zero");
 
-            
+
             var result = await _unitOfWork.loanService.ApplyLoanPayment(ph.CustomerLoanId, ph.AmountPaid, ph.fkPaidBy);
 
             if (result == null) return NotFound("Loan not found");
 
+            return Ok(result);
+        }
+
+
+        [HttpGet]
+        [Route("GetAllLoanDetail")]
+        public async Task<List<Loan>> GetDetails() => await _unitOfWork.loanService.GetDetails();
+
+
+        [HttpPatch]
+        [Route("Updateloandetail")]
+        public async Task<IActionResult> UpdateDetails(Loan loan)
+        {
+            if (!ModelState.IsValid) return ValidationProblem(ModelState);
+            var result = await _unitOfWork.loanService.UpdateDetails(loan);
             return Ok(result);
         }
 
